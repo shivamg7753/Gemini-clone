8219bcc42f4bd12155e2447b4b11d2d63cdf5a6d
shivam7753 <shivamgupta.ece.21@nitap.ac.in>
2024-12-11 14:45:23 +0530

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,55 @@
+import React, { useContext } from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+const Main = () => {
+
+  const {onSent,recentPrompt,showResult,loading,resultData,setInput,input}=useContext(Context)
+
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        {!showResult?<>
+          <div className="greet">
+          <p><span>hello, dev.</span></p>
+          <p>How can I help you today?</p>
+        </div>
+        </>: <div className="result">
+          <div className="result-title">
+            <img src={assets.user_icon} alt="" />
+            <p>{recentPrompt}</p>
+          </div>
+          <div className="result-data">
+            <img src={assets.gemini_icon} alt="" />
+            {loading? <div className='loader'>
+              <hr />
+              <hr />
+              <hr />
+            </div>:<p dangerouslySetInnerHTML={{ __html: resultData }} />}
+            
+            </div>
+        </div> }
+        
+        <div className="main-bottom">
+          <div className="search-box">
+            <input onChange={(e)=>setInput(e.target.value)} value = {input} type="text" placeholder='Enter a prompt here' />
+           <div>
+             <img src={assets.gallery_icon} alt="" />
+             <img src={assets.mic_icon} alt="" />
+            {input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null} 
+           </div>
+          </div>
+          <p className="bottom-info">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi a eveniet, minima adipi
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Main
